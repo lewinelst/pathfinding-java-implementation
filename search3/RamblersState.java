b2ab7a42ba212f77355e01dd4310543b08ece6b8
Lewin <lewinelst@gmail.com>
2021-05-05 17:05:11 +0100

    branch and bound working

--- search3/RamblersState.java
+++ search3/RamblersState.java
@@ -16,7 +16,7 @@ public class RamblersState extends SearchState{
     public boolean goalPredicate(Search searcher){ // Checks if current state is the goal
         RamblersSearch ramSearcher = (RamblersSearch) searcher;
         Coords goal = ramSearcher.returnGoal();
-        if (goal == stateCoord){
+        if ((goal.getx() == stateCoord.getx()) && (goal.gety() == stateCoord.gety())){
             return true;
         } else {
             return false;
@@ -24,7 +24,8 @@ public class RamblersState extends SearchState{
     }
 
     public boolean sameState(SearchState stateTwo){
-        if (this == stateTwo){
+        RamblersState ramblersStateTwo = (RamblersState)stateTwo;
+        if ((this.getLocalCost() == stateTwo.getLocalCost() && (this.stateCoord.getx() == ramblersStateTwo.getCoords().getx()) && (this.stateCoord.gety() == ramblersStateTwo.getCoords().gety()))){
             return true;
         } else {
             return false;
@@ -40,28 +41,28 @@ public class RamblersState extends SearchState{
         int y = stateCoord.gety();
         int tmap[][] = map.getTmap();
         //North
-        if((y -= 1) >= 0){
-            Coords newCoordinates = new Coords(y -= 1, x);
+        if((y - 1) >= 0){
+            Coords newCoordinates = new Coords(y - 1, x);
             RamblersState newSuccessor = new RamblersState(newCoordinates, tmap[newCoordinates.gety()][newCoordinates.getx()]);
             successors.add(newSuccessor);
         }
         //East
-        if((x += 1) <= map.getWidth()){
-            Coords newCoordinates = new Coords(y, x += 1);
+        if((x + 1) <= map.getWidth()){
+            Coords newCoordinates = new Coords(y, x + 1);
             RamblersState newSuccessor = new RamblersState(newCoordinates, tmap[newCoordinates.gety()][newCoordinates.getx()]);
             successors.add(newSuccessor);
         }
 
         //South
-        if((y += 1) <= map.getHeight()){
-            Coords newCoordinates = new Coords(y += 1, x);
+        if((y + 1) <= map.getHeight()){
+            Coords newCoordinates = new Coords(y + 1, x);
             RamblersState newSuccessor = new RamblersState(newCoordinates, tmap[newCoordinates.gety()][newCoordinates.getx()]);
             successors.add(newSuccessor);
         }
 
         //West
-        if((x -= 1) >= 0){
-            Coords newCoordinates = new Coords(y, x -= 1);
+        if((x - 1) >= 0){
+            Coords newCoordinates = new Coords(y, x - 1);
             RamblersState newSuccessor = new RamblersState(newCoordinates, tmap[newCoordinates.gety()][newCoordinates.getx()]);
             successors.add(newSuccessor);
         }
@@ -70,7 +71,7 @@ public class RamblersState extends SearchState{
     }
 
     public String toString(){
-        return ("Ramblers State: " + stateCoord.toString());
+        return ("Ramblers State: " + "(" + stateCoord.getx() + "," + stateCoord.gety() + ")");
     }
 
 
