7c71176987c0ac2bce1f2f977be65855dde2b8ec
Lewin <lewinelst@gmail.com>
2021-05-05 16:39:00 +0100

    first attempt at getting search working

--- search3/RunMapSearch.java
+++ search3/RunMapSearch.java
@@ -1,6 +1,13 @@
 public class RunMapSearch {
-    TerrainMap map = new TerrainMap("tmc.pgm");
-    Coords goal = new Coords(8, 5);
-
-    RamblersSearch search = new RamblersSearch(map, goal);
+    public static void main(String[] args){
+        TerrainMap map = new TerrainMap("search3/tmc.pgm");
+        Coords goal = new Coords(5, 8);
+    
+        int tmap[][] = map.getTmap();
+    
+        RamblersSearch search = new RamblersSearch(map, goal);
+        SearchState startState = (SearchState) new RamblersState(new Coords(0, 7), tmap[0][7]);
+        String output = search.runSearch(startState, "breadthFirst");
+        System.out.println(output);
+    }
 }
